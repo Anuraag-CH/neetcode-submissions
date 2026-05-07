1ee807aba487c0fbf70ff008d6b5c99170d80292
Anuraag-CH <48093039+Anuraag-CH@users.noreply.github.com>
2026-05-07 09:38:01 +0530

    Add: can-place-flowers - submission-7

---
+++ Data Structures & Algorithms/can-place-flowers/submission-7.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+
+        count = 0
+
+        if len(flowerbed) == 1:
+            if flowerbed[0] == 0:
+                return n <= 1
+            else:
+                return n <= 0
+
+        for i in range(0, len(flowerbed)):
+            if i == 0:
+                if flowerbed[i] == 0 and flowerbed[i + 1] == 0:
+                    flowerbed[i] = 1
+                    count += 1
+
+            elif i == len(flowerbed) - 1:
+                if flowerbed[i] == 0 and flowerbed[i - 1] == 0:
+                    flowerbed[i] = 1
+                    count += 1
+
+            else:
+                if flowerbed[i] == 0 and flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0:
+                    flowerbed[i] = 1
+                    count += 1
+
+        return n <= count
