5d911bd442fb409a6599d4699caccfcd2423fc49
Anuraag-CH <48093039+Anuraag-CH@users.noreply.github.com>
2026-05-22 03:49:35 +0530

    Add: kth-distinct-string-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-distinct-string-in-an-array/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+
+        hash_set = {}
+
+        for a in arr:
+            hash_set[a] = hash_set.get(a, 0) + 1
+
+        for a in arr:
+            if hash_set[a] == 1:
+                k -= 1
+
+            if k == 0:
+                return a
+
+        return ""
